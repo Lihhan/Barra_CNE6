1eeb8dc5165c5482e919d459a0dbeb64d4b45d13
Lihan <112416160+Accepted1898@users.noreply.github.com>
2024-08-14 14:09:09 +0800

    Create globalVariables.py

---
+++ globalVariables.py
@@ -0,0 +1,12 @@
+START_YEAR = 2017
+END_YEAR = 2024
+
+BENCHMARK = '000001.SH'
+BENCH = '000001'
+SENTINEL = 1e10
+INDUSTRYNUM = 29
+short_time_HALFLIFE = 63
+long_time_HALFLIFE = 260
+HALFLIFE = 63
+short_time_window = 252
+long_time_window = 1040
